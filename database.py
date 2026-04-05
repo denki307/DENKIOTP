cd31d8143d6850ef124ed9b1d236d3f3a61d67bd
denki307 <200189507+denki307@users.noreply.github.com>
2026-04-05 06:43:16 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from pymongo import MongoClient
 
-# KANDIPPA INGA UNGA MONGODB ATLAS URL-A PODANUM
 MONGO_URL = "mongodb+srv://Devilsirophai:devilbhaiontop@devil0.d9epxqw.mongodb.net/?appName=Devil0"
 
 client = MongoClient(MONGO_URL)
@@ -9,7 +8,6 @@ db = client["TelegramStore"]
 users_db = db["users"]
 accounts_db = db["accounts"]
 
-# --- User Balance Functions ---
 def get_balance(user_id):
     user = users_db.find_one({"user_id": user_id})
     return user["balance"] if user else 0.0
@@ -28,7 +26,6 @@ def set_balance(user_id, amount):
         upsert=True
     )
 
-# --- Account Stock Functions ---
 def add_account(country, acc_details):
     accounts_db.insert_one({"country": country, "details": acc_details})
 
@@ -36,10 +33,8 @@ def get_stock_count(country):
     return accounts_db.count_documents({"country": country})
 
 def get_all_countries():
-    # Ethana countries stock la irukku nu list edukkum
     return accounts_db.distinct("country")
 
 def get_and_remove_account(country):
     acc = accounts_db.find_one_and_delete({"country": country})
     return acc["details"] if acc else None
-
